347d1a41b05422f861a08ddd3d38b64906f476f2
Aks132 <achyutsapariya@gmail.com>
2021-10-01 15:59:08 +0530

    excel edit bug fox

--- api/function/excelread.py
+++ api/function/excelread.py
@@ -4,7 +4,7 @@ from Robo_inv_managment.settings import TEMPLATE_DIR
 
 def readsheet():
     sheet = pd.read_excel('ROBO INV.xlsx', index_col=0,skiprows=1)
-    PATH = os.path.join(TEMPLATE_DIR, 'test.html')
+    PATH = os.path.join(TEMPLATE_DIR,'test.html')
     print(PATH)
 
     sheet.to_html(PATH)
